c35885b1ae6b4e21e099f58ef0c00f1c1e32a745
dymafr <erwan1309@hotmail.com>
2022-03-30 10:47:49 +0200

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import Boutique from './features/boutique/Boutique.vue';
 import Admin from './features/admin/Admin.vue';
 import { reactive, type Component as C } from 'vue';
 import type { Page } from './interfaces';
-import { seed } from './data/seed';
+import { seed40articles } from './data/seed';
 
 const state = reactive<{
     page: Page
@@ -22,7 +22,7 @@ function navigate(page: Page): void {
     state.page = page;
 }
 
-// seed('projetproducts');
+// seed40articles('projetproducts');
 </script>
 
 <template>
